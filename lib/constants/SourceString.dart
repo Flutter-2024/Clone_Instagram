d75d7c40b6a039f0cc48a8afd1ff4a349dd7af3f
mdunggggg <dungmanh74@gmail.com>
2024-01-22 14:17:19 +0700

    [IS20] Add Hamburger and BottomSheet for change account

--- lib/constants/SourceString.dart
+++ lib/constants/SourceString.dart
@@ -1,5 +1,8 @@
-
 class SourceString{
+  static const String userName = "_mdunggggg_";
+  static const String addNewAccount = "Thêm tài khoản Instagram";
+  static const String yourProfileInFacebook = "Trang cá nhân của bạn trên Facebook";
+  static const String accountCenter = "Đi đến Trung tâm tài khoản";
   static const String appName = "Clone Instagram";
   static const String titleAppBar = "Instagram";
   static const String home = "Home";
---
+++ lib/pages/profile/profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:clone_instagram/pages/profile/profile_app_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget{
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: const [
+              SizedBox(height: 20),
+              ProfileAppBar(),
+            ],
+          )
+        )
+    );
+  }
+
+}
